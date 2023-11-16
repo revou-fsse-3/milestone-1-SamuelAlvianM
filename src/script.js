b677894398e6fa113ffdd27b6f83f9e4790bae40
SamuelAlvianM <oensam07@gmail.com>
2023-11-16 19:06:53 +0700

    update CSS HTML JS

--- src/script.js
+++ src/script.js
@@ -1,8 +1,38 @@
-// sticky navbar
+// make toggle for hamburger
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.nav-links');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x-circle');
+    navbar.classList.toggle('active');
+};
+
+
+// sticky navbar &scroll section active link
+const sections =document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
+
 window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute ('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(Links => {
+                Links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
 let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
+
+menuIcon.classList.remove('bx-x-circle');
+navbar.classList.remove('active');
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -18,7 +48,4 @@ document.addEventListener('DOMContentLoaded', function () {
             index = (index + 1) % words.length;
         }, 500);
     }
-
-    // Ganti kata setiap 3 detik
-    setInterval(changeWord, 2000);
 });
